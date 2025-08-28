5db41f6fe65b499d89f1471c0acd5880b50c0f51
mobinmiah <mobinmiah1990@gmail.com>
2025-08-28 07:07:27 +0600

    js file added

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,33 @@
+// ids
+const hearts = document.getElementById("heart-counter")
 const coins = document.getElementById("coin-counter")
-console.log(parseInt(coins.innerText))
+const copies = document.getElementById("copy-counter")
+
+// from card national
+const nationalHeartBtn = document.getElementById("national-heart-btn")
+const nationalCallNumber = document.getElementById("national-call-number")
+const nationalCategory = document.getElementById("national-card-category")
+const nationalCopyBtn = document.getElementById("national-copy-btn")
+const nationalCallBtn = document.getElementById("national-call-btn")
+
+// from cards police
+const policeHeartBtn = document.getElementById("police-heart-btn")
+const policeCallNumber = document.getElementById("policeCallNumber")
+const policeCategory = document.getElementById("policeCategory")
+const policeCopyBtn = document.getElementById("police-copy-btn")
+const policeCallBtn = document.getElementById("police-call-btn")
+
+// from cards national
+const fireHeartBtn = document.getElementById("fire-heart-btn")
+const fireCallNumber = document.getElementById("fire-call-number")
+const fireCategory = document.getElementById("fire-category")
+const fireCopyBtn = document.getElementById("fire-copy-btn")
+const fireCallBtn = document.getElementById("fire-call-btn")
+
+// from cards ambulance
+const ambulanceHeartBtn = document.getElementById("ambulance-heart-btn")
+
+// classes
+const cardBtns = document.getElementsByClassName("card-btn")
+const copyBtns = document.getElementsByClassName("copy-btn")
+const callBtns = document.getElementsByClassName("call-btn")
