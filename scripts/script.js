0d80196548414e6b64b2d821549488853235396b
mobinmiah <mobinmiah1990@gmail.com>
2025-08-29 05:54:22 +0600

    fighting for appendchild

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ const callBtns = document.querySelectorAll(".call-btn")
 // call history
 const callHistoryBtn = document.getElementById("call-history-btn")
 const callHistoryContainer = document.getElementById("call-history-container")
+
 const callHistory = [];
 
 // heart btn functionalities
@@ -29,14 +30,14 @@ for (btn of cardBtns) {
 let countCoins = 100;
 for (const btn of callBtns) {
     btn.addEventListener("click", function () {
-        countCoins = countCoins - 20;
-        coins.innerText = countCoins;
-        const coinNumber = parseInt(coins.innerText)
 
-        if (coinNumber < 20) {
+        if (countCoins < 20) {
             alert("You Don't Have Sufficient Balance");
             return;
         }
+
+        countCoins = countCoins - 20;
+        coins.innerText = countCoins;
 
         const cardT = btn.parentElement.parentElement;
         const title = cardT.querySelector(".card-title")
@@ -44,11 +45,42 @@ for (const btn of callBtns) {
 
         const cardN = btn.parentElement.parentElement;
         const number = cardN.querySelector(".card-numbers")
-        const copyCardNumber = number.innerText;
-        alert("📞 Calling... " + cardTitle + " " + copyCardNumber)
+        const cardNumber = number.innerText;
+        alert("📞 Calling... " + cardTitle + " " + cardNumber)
 
+        const data = {
+            name: cardTitle,
+            number: cardNumber,
+            date: new Date().toLocaleTimeString()
+        }
+        callHistory.push(data)
+
+        callHistoryContainer.innerHTML = "";
+        for (const call of callHistory) {
+            const div = document.createElement("div")
+            div.innerHTML = 
+            `
+            <div class="call flex justify-between items-center mx-6 mt-4 p-4 rounded-[8px] bg-[#FAFAFA]">
+                    <div>
+                        <h3 class="service-name font-inter font-semibold text-[1.25rem] dark-color">Fire Service Number
+                        </h3>
+                        <p class="service-number font-madurai text-[1.25rem] gray-color">999</p>
+                    </div>
+                    <p class="time font-madurai text-[1.125rem] dark-color">
+                        11:33:55 AM
+                    </p>
+                </div>
+            `
+            callHistoryContainer.appendChild(div)
+            console.log(div)
+        }
     })
 }
+
+
+
+
+
 
 // copy btn functionalities
 let countCopies = 0;
